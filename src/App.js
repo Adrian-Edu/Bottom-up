8a8dd86c8eafcdc2139ff6ed19d65527e0d14e50
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2022-07-06 06:28:13 +0300

    bun

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Product from './import';
 
-function App() {
+function Candidat() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Salut dragilor.</h1>
+
+    name = "Edu"
+    prenume = "Adrian"
+    varsta = "33"
+    oras = "Bucuresti"
+
+
+    <Product 
+    name = "Edu"
+    prenume = "Adrian"
+    varsta = "33"
+    oras = "Bucuresti"
+    />
+  
     </div>
-  );
+  )
 }
 
-export default App;
+export default Candidat;
---
+++ src/import.jsx
@@ -0,0 +1,16 @@
+
+function Product (props) {
+
+
+    return (
+        <div>
+            <p> {props.name} </p>
+            <p> {props.prenume} </p>
+            <p> {props.varsta} </p>
+            <p> {props.oras} </p>
+            </div>
+    )
+
+}
+
+export default Product
